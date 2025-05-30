99004db616c212c0cfcf201d641a744078729775
Jeremiah <157841672+Aylos9er@users.noreply.github.com>
2025-05-30 05:33:34 -0700

    Add .next/server/app/api/git-push/route.js 1748608414128

---
+++ .next/server/app/api/git-push/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/git-push/route.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_fd748c74._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__d07a5e0e._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/git-push/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/git-push/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/git-push/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
